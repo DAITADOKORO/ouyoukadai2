6e76fcbb8e59ae0457c89585a086f68427c33d66
DAITADOKORO <bangasa0101@gmail.com>
2019-12-25 17:08:40 +0900

    finished kadai3 retry

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -11,6 +11,15 @@ class BookCommentsController < ApplicationController
         render 'books/show'
       end
   end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    book_comment = current_user.book_comments.find(params[:id])
+    book_comment.book_id = book.id
+    book_comment.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   def book_comment_params
       params.require(:book_comment).permit(:user_id,:book_id,:comment)
