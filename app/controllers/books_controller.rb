ea24fab73a7775bcc799d4d4531faca7b8412ccc
DAITADOKORO <bangasa0101@gmail.com>
2019-12-25 12:38:55 +0900

    finished ouyoukadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,35 @@
 class BooksController < ApplicationController
-	before_action :authenticate_user!
-	#ビフォーアクションはこのコントローラーが実行される前という意味
-	#authenticate userはdeviseで用意されているメソッドで、ログイン認証がされていないとrootパスにリダイレクトされる。
-	#アプリケーションコントローラーに記述すれば全適用されるが、今回はhomeコントローラーは適用したくないので、booksとusersに適用
+  before_action :authenticate_user!
 
   def show
   	@book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
   end
 
   def index
-  	@book = Book.new #new bookの新規投稿で必要、ブックモデルに新しいデータを渡すのでnew
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	@book.user_id = current_user.id #カレント（ログインしてる人）idをbook user id変数として格納する。
+    @book.user_id = current_user.id
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
-  		render 'index'
+      @user = current_user
+  		render :index
   	end
   end
 
   def edit
   	@book = Book.find(params[:id])
-  	if @book.user.id != current_user.id
-  		redirect_to books_path
-  	end
+    @user = User.find(@book.user_id)
+    if @user != current_user
+      redirect_to books_path
+    end
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
-	#ビフォーアクションはこのコントローラーが実行される前という意味
-	#authenticate userはdeviseで用意されているメソッドで、ログイン認証がされていないとrootパスにリダイレクトされる。
-	#アプリケーションコントローラーに記述すれば全適用されるが、今回はhomeコントローラーは適用したくないので、booksとusersに適用
-	before_action :baria_user, only: [:edit, :update]
+  before_action :authenticate_user!
+
+	before_action :baria_user, only: [:update]
 
   def show
   	@user = User.find(params[:id])
@@ -18,14 +16,17 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
-  		redirect_to user_path(@user), notice: "successfully updated user!"
+  		redirect_to user_path(@user.id), notice: "successfully updated user!"
   	else
-  		render "edit"
+  		render :edit
   	end
   end
 
